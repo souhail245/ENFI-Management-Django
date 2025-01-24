04f3c31a017a5b2e8396c89def7e5d4828af501a
Castro <castrobusiness2015@gmail.com>
2025-01-24 19:02:15 +0100

    qlq ameliorations f emploitemsp + fixer problem student espace logging

--- main_app/student_views.py
+++ main_app/student_views.py
@@ -64,18 +64,11 @@ def export_attestation_pdf(request, student_id):
 
 def student_home(request):
     student = get_object_or_404(Student, admin=request.user)
-    total_subject = Subject.objects.filter(course=student.course).count()
-    total_attendance = AttendanceReport.objects.filter(student=student).count()
-    total_present = AttendanceReport.objects.filter(student=student, status=True).count()
-    if total_attendance == 0:  # Don't divide. DivisionByZero
-        percent_absent = percent_present = 0
-    else:
-        percent_present = math.floor((total_present/total_attendance) * 100)
-        percent_absent = math.ceil(100 - percent_present)
+    total_subject = Subject.objects.filter(niveau=student.niveau).count()
     subject_name = []
     data_present = []
     data_absent = []
-    subjects = Subject.objects.filter(course=student.course)
+    subjects = Subject.objects.filter(niveau=student.niveau)
     for subject in subjects:
         attendance = Attendance.objects.filter(subject=subject)
         present_count = AttendanceReport.objects.filter(
@@ -86,9 +79,8 @@ def student_home(request):
         data_present.append(present_count)
         data_absent.append(absent_count)
     context = {
-        'total_attendance': total_attendance,
-        'percent_present': percent_present,
-        'percent_absent': percent_absent,
+        
+        
         'total_subject': total_subject,
         'subjects': subjects,
         'data_present': data_present,
